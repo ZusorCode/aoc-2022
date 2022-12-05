a66422c2795dbee129704bd96e704abc830abe85
Tobias Messner <tobias@zusor.io>
2022-12-05 15:36:47 +0100

    Add day 5 and update aocd

---
+++ day5/day5a.py
@@ -0,0 +1,19 @@
+from aocd import data, submit
+
+stacks_diagram, instructions = data.split("\n\n")
+stack_count = int(stacks_diagram.split("\n")[-1].strip()[-1])
+
+stacks = {}
+for stack_num in range(stack_count):
+    stacks[stack_num] = [row[1 + stack_num * 4] for row in stacks_diagram.split("\n")[:-1]]
+    stacks[stack_num] = [letter for letter in stacks[stack_num] if letter != " "]
+
+for instruction in instructions.split("\n"):
+    _, count, _, start, _, target = instruction.split(" ")
+    count, start, target = int(count), int(start) - 1, int(target) - 1
+    for i in range(count):
+        stacks[target].insert(0, stacks[start].pop(0))
+
+output = "".join([stack[0] for stack in stacks.values()])
+print(output)
+submit(output)
